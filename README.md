8d6e9534b093b3e009341ba9cc0275128234c0b6
yunji-choi <true665998@gmail.com>
2024-11-05 11:44:49 +0900

    change image

---
+++ README.md
@@ -0,0 +1,59 @@
+# Ejel Chat Demo for Vanilla JS
+
+이 프로젝트는 `ejel-chat-js` 라이브러리를 사용한 기본적인 바닐라 자바스크립트 데모입니다.
+
+![demo](./public/screenshot-demo.png)
+
+## 기능
+
+이 데모에서는 [Room](https://docs.ejelai.com/room)을 사용하여 다음과 같은 기능을 구현합니다.
+
+- Ejel Chat stream 참여/종료
+- 에이전트와 텍스트 채팅으로 대화
+- 에이전트 비디오 추가/제거
+- 음성 녹음
+- STT(Speech-to-Text)
+
+각 기능에 대한 자세한 사항은 [Documentation](https://docs.ejelai.com)에서 확인하세요.
+
+## 시작하기
+
+### 사전 요구사항
+
+- Ejel API Key (https://ejelai.com/api-key)
+- npm 또는 yarn
+
+### 설치
+
+1. 저장소를 클론합니다
+
+```bash
+git clone https://github.com/VMONSTER-AI/ejel-chat-js-vanilla-demo.git
+cd vanilla-demo
+```
+
+2. 의존성 설치
+
+```bash
+npm install
+# 또는
+yarn install
+```
+
+3. 환경 변수 설정
+
+```bash
+VITE_API_KEY=YOUR_API_KEY  // Ejel API Key
+VITE_AGENT_ID=YOUR_AGENT_ID  // Ejel Agent ID
+VITE_SERVER_URL=YOUR_SERVER_URL  // 기본적으로 http://api.ejelai.com 를 사용하세요.
+```
+
+1. 애플리케이션을 시작합니다:
+
+```bash
+npm run dev
+# 또는
+yarn run dev
+```
+
+이제 개발 서버가 localhost:3001에서 실행됩니다.
---
+++ main.js
@@ -0,0 +1,128 @@
+import Room from "ejel-chat-js";
+
+const room = new Room({
+  // Please set agentId, apiKey, and serverUrl values in the .env file
+  agentId: import.meta.env.VITE_AGENT_ID,
+  apiKey: import.meta.env.VITE_API_KEY,
+  serverUrl: import.meta.env.VITE_SERVER_URL,
+});
+
+let sttResult = "";
+
+const videoStyle = {
+  borderRadius: "20px",
+  width: "100%",
+  height: "100%",
+  backgroundColor: "none",
+  objectFit: "cover",
+};
+
+function logClient(text) {
+  console.log(`[Client] - ${text}`);
+}
+
+function updateJoinStatus(isJoined) {
+  document.getElementById("joinStatus").textContent = isJoined
+    ? "Joined!"
+    : "Not Joined..";
+}
+
+function updateSpeakingStatus(isAgentSpeaking) {
+  document.getElementById("speakingStatus").textContent = isAgentSpeaking
+    ? "Agent is Speaking!"
+    : "Agent is not Speaking..";
+}
+
+function setupEventListeners() {
+  room.on("started", () => {
+    updateJoinStatus(true);
+    updateSpeakingStatus(false);
+  });
+
+  room.on("agent-message", (msg) => {
+    logClient(msg);
+  });
+
+  room.on("agent-start-speaking", () => {
+    updateSpeakingStatus(true);
+  });
+
+  room.on("agent-stop-speaking", () => {
+    updateSpeakingStatus(false);
+  });
+
+  room.on("closed", () => {
+    logClient("connection closed");
+    updateJoinStatus(false);
+    updateSpeakingStatus(false);
+    document.getElementById("chatInput").value = "";
+    document.getElementById("sttResult").style.display = "none";
+    sttResult = "";
+  });
+}
+
+document.getElementById("joinBtn").addEventListener("click", async () => {
+  // To add a video with a specific style, call room.addVideo(style) before join()
+  room.addVideo(videoStyle);
+
+  setupEventListeners();
+  try {
+    await room.join();
+  } catch (error) {
+    logClient("join error..");
+    console.error(error);
+  }
+});
+
+document.getElementById("chatBtn").addEventListener("click", async () => {
+  const text = document.getElementById("chatInput").value;
+  try {
+    await room.chat(text);
+    document.getElementById("chatInput").value = "";
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+document.getElementById("leaveBtn").addEventListener("click", () => {
+  room.leave();
+});
+
+document.getElementById("addVideoBtn").addEventListener("click", () => {
+  room.addVideo(videoStyle);
+});
+
+document.getElementById("removeVideoBtn").addEventListener("click", () => {
+  room.removeVideo();
+});
+
+document.getElementById("startRecordBtn").addEventListener("click", () => {
+  room.startRecordingAudio();
+});
+
+document.getElementById("stopRecordBtn").addEventListener("click", async () => {
+  try {
+    const blob = await room.stopRecordingAudio();
+    const text = await room.stt(blob, "ko");
+    sttResult = text;
+    if (sttResult.trim() !== "") {
+      document.getElementById("sttText").textContent = text;
+      document.getElementById("sttResult").style.display = "block";
+    }
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+document
+  .getElementById("sendChatWithSttResultBtn")
+  .addEventListener("click", async () => {
+    logClient(sttResult);
+    try {
+      await room.chat(sttResult);
+      document.getElementById("sttResult").style.display = "none";
+      sttResult = "";
+    } catch (error) {
+      console.error("[Client] - " + error);
+    }
+  });
